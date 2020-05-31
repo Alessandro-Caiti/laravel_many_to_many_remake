a03eb3a14731abdfc49ef8e2b8d4f968b5c10385
Alessandro Caiti <acaiti@hotmail.it>
2020-05-31 18:09:55 +0200

    controller page and create blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Category;
+use App\InfoUser;
+use App\Page;
+use App\Photo;
+use App\Tag;
+use App\User;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +30,14 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $user = User::find(1);
+        $page = Page::find(1);
+        // dd($user->info->bio);
+        // dd($user->categories);
+        // dd($user->photos);
+        // dd($user->tags);
+        // dd($user->pages);
+        dd($page->tags);
         return view('home');
     }
 }
